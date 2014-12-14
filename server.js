7ed0183fd73a5d549f699decbce8ceb0ddea5072
dchippendale <dchippendale@gmail.com>
2014-12-14 14:02:38 -0500

    restructured packages.json

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/target'));
 
-// app.get('/', function(request, response) {
-//   response.send('Hello World!');
-// });
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
 
-// app.listen(app.get('port'), function() {
-//   console.log("Node app is running at localhost:" + app.get('port'));
-// });
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
