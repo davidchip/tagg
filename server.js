2a19f91d6c775e2473ba156f73989b7d4f2543a4
dchippendale <dchippendale@gmail.com>
2014-12-14 14:50:59 -0500

    removed static

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 8000));
-app.use('/static', express.static(__dirname + '/target'));
+app.use(express.static(__dirname + '/target'));
 
 // app.get('/', function(request, response) {
 //   response.send('Hello World!');
