f4210c80ace1d2769026384c62a399ac8d97dcb8
dchippendale <dchippendale@gmail.com>
2014-12-14 14:13:19 -0500

    configured for target

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use('/', express.static(__dirname + '/target'));
+app.use('/target', express.static(__dirname + '/target'));
 
 // app.get('/', function(request, response) {
 //   response.send('Hello World!');
