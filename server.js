4c04558c00fb7dd77a3bb81209b446cf06dd2f42
dchippendale <dchippendale@gmail.com>
2014-12-14 14:24:56 -0500

    added port listening

--- server.js
+++ server.js
@@ -8,6 +8,4 @@ app.use(express.static(__dirname + '/target'));
 //   response.send('Hello World!');
 // });
 
-// app.listen(app.get('port'), function() {
-//   console.log("Node app is running at localhost:" + app.get('port'));
-// });
+app.listen(app.get('port'));
